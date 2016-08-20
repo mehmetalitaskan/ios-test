781d7782e16d64b49a25fc95c1dc36821d1f4536
Mehmet ali Taşkan <MehmetAliTaskan@Mehmets-MacBook-Pro.local>
2016-08-20 23:08:38 +0300

    Initial Commit

---
+++ merhabadunya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  merhabadunya
+//
+//  Created by Mehmet ali Taşkan on 20/08/16.
+//  Copyright (c) 2016 Mehmet ali Taşkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
